75a3919dfe02ca700abe5cd9f68eeed02948ef75
Charles Arciniega & Nina Hwang <pair+arcin.nihwang@devbootcamp.com>
2014-04-11 16:43:26 -0700

    adds bcrypt functionality to post routes

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -3,9 +3,22 @@ get "/session/login" do
 end
 
 post "/session/login" do
+  user = User.find_by_username(params[:username])
 
+  if user.password == BCrypt::Engine.hash_secret(params[:password], user.salt)
+    "you win"
+  else
+    "you lose"
+  end
 end
 
 post "/session/sign-up" do
-
+  salt = BCrypt::Engine.generate_salt
+  User.create(
+    username: params[:username],
+    email: params[:email],
+    password: BCrypt::Engine.hash_secret(params[:password], salt),
+    password_salt: salt
+    )
+  redirect '/'
 end
