f58d1ceb5084c8d9299b95d59e9cd55bac989c52
Charles Arciniega <charlesarcin@gmail.com>
2014-04-12 20:54:23 -0700

    Changes Associations between user and surveys 	* had to alias the tablename in the associations as 'interactions' instead of 'survey_interactions' in order to get the association to work and link a survey to a user 	* TODO: change migrations so that Users have many questions. This way we wont run into errors when multiple users are dynamically creating questions/surveys   * had to change

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,6 +1,6 @@
 class Survey < ActiveRecord::Base
-  has_many :survey_interactions
+  has_many :interactions, :foreign_key => 'survey_id', :class_name => "SurveyInteraction"
+  has_many :users, through: :interactions
   has_many :questions
-  has_many :users, through: :survey_interactions
   # Remember to create a migration!
 end
